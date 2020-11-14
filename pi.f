fadf3d8da7f8f7556744467de501bbd050825d81
hkershaw-brown <20047007+hkershaw-brown@users.noreply.github.com>
2020-11-13 20:50:03 -0700

    Update and rename pi.f90 to pi.f

---
+++ pi.f
@@ -0,0 +1,28 @@
+C Program to compute Pi using monte carlo methods
+
+       program monte_pi
+       implicit none
+       
+       integer niter,i,j
+       integer seed 
+       real*4 count
+       real *8 x,y,pi(100),z
+       real*8   rand
+       
+C initialize random numbers
+       seed = 35791246
+       call srand (seed)
+       do j= 1,100
+          niter = niter+100
+          count =0
+          do i=1,niter
+             x=rand()
+             y=rand()
+             z= x*x +y*y
+             if (z .le. 1) count =count+1
+          end do
+          pi(j)= count/niter*4.
+          write(*,10) niter,pi(j)
+10        format('Number of trials is: 'i5,'  estimate of pi is:',f8.5)
+       end do
+       end
